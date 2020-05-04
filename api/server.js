725280f2dc1df8c931268146f396ebc4434a5bab
mikejeter <mjeter45@yahoo.com>
2020-05-03 21:44:34 -0400

    Added sessions and cookies for authentication system

--- api/server.js
+++ api/server.js
@@ -1,17 +1,43 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-
+const session = require('express-session');
+const knexSessionStore = require('connect-session-knex')(session);
 const authenticate = require('../auth/authenticate-middleware.js');
+
+
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
 
 const server = express();
 
+const sessionConfig = {
+    name: 'auth-challenge',
+    secret: 'myhsdbcjaeieutysecret',
+    cookie: {
+        maxAge: 3600 * 1000,
+        secure: false,  // should be true in production
+        httpOnly: true
+    },
+    resave: false,
+    saveUninitialized: false,
+
+    store: new knexSessionStore(
+      {
+        knex: require("../database/dbConfig.js"),
+        tablename: "sessions",
+        sidfieldname: "sid",
+        createtable: true,
+        clearInterval: 3600 * 1000
+
+      }
+    )
+};
+
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-
+server.use(session(sessionConfig));
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
 
